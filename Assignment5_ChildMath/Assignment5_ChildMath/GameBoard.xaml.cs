4ac83444d761781ea14b17f8f787653149897243
johnwyke <johnwyke@mail.weber.edu>
2020-12-21 13:05:16 -0700

    Initial

---
+++ Assignment5_ChildMath/Assignment5_ChildMath/GameBoard.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Assignment5_ChildMath
+{
+    /// <summary>
+    /// Interaction logic for GameBoard.xaml
+    /// </summary>
+    public partial class GameBoard : Window
+    {
+        
+        FinalScore wpfFinal;
+        private GameRules GAME = new GameRules();
+        DispatcherTimer Timer = new DispatcherTimer();
+        private bool timerStart = true;
+        private int lengthTime;
+        private int playerGuess;
+        private int QuestionCount=10;
+        public GameBoard()
+        {
+            InitializeComponent();
+           // wpfMainMenu = new MainWindow();
+            wpfFinal = new FinalScore();
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Tick += Timer_Tick;
+        }
+        /// <summary>
+        /// Counts and Displays  counter
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Timer_Tick(object sender, EventArgs e)
+        {  
+            if (timerStart)
+            {
+                lengthTime = 0;
+                timerStart = false;
+            }
+            else
+                lengthTime += 1;
+            //lblTimer.Content = DateTime.Now.Second;
+            lblTimer.Content = lengthTime;
+        }
+        /// <summary>
+        /// Calls methods to display numbers resets for next question
+        /// </summary>
+        private void nextQuestion()
+        {
+            if (QuestionCount > 0)
+            {
+                GAME.MathFunction();
+                SetNumOne();
+                SetNumTwo();
+                txtGuess.Text = "";
+                QuestionCount--;
+            }
+            else
+            {
+                btnStart.IsEnabled = false;
+                this.Hide();
+                wpfFinal.ShowDialog();
+
+            }
+        }
+        /// <summary>
+        /// Gets and displays Numer one
+        /// </summary>
+        private void SetNumOne() { lblNum1.Content = GameRules.iNumberOne; }
+        /// <summary>
+        /// Gets and displays number two
+        /// </summary>
+        private void SetNumTwo() { lblNum2.Content = GameRules.iNumberTwo; }
+        /// <summary>
+        /// Set and Sidplay GameMode
+        /// </summary>
+        private void SetSymb() { lblSym.Content = GameRules.gameMode; }
+        /// <summary>
+        /// Cycles the questions
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(txtGuess.Text, out playerGuess);
+            GAME.correctAnswer(playerGuess);
+            nextQuestion();
+        }
+        /// <summary>
+        /// Starts the Timer on the Window Load
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Timer.Start();
+            SetSymb();
+            nextQuestion();
+        }
+
+        private void txtGuess_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key <= Key.D0 || e.Key >= Key.D9)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+        }
+
+    }
+}
---
+++ Assignment5_ChildMath/Assignment5_ChildMath/GameRules.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5_ChildMath
+{
+    /// <summary>
+    /// The Background making the game work.
+    /// </summary>
+    public class GameRules
+    {
+        private int iAnswer;
+        
+        /// <summary>
+        /// What Game Mode Player Chose.
+        /// </summary>
+        public static char gameMode { get; set; }
+        /// <summary>
+        /// First Number in the Equation.
+        /// </summary>
+        public static int iNumberOne { get; set; }
+        /// <summary>
+        /// Second Number in the Equation
+        /// </summary>
+        public static int iNumberTwo { get; set; }
+        /// <summary>
+        /// This is the players guess 
+        /// </summary>
+        public static int iGuess {get;set;}       
+
+        /// <summary>
+        /// Generate a random number 
+        /// </summary>
+        /// <returns></returns>
+        public void GenerateNumbers()
+        {
+            Random RNDNUM = new Random();
+            iNumberOne = RNDNUM.Next(0, 10);
+
+            if (gameMode == '/')
+            {               
+                iNumberTwo = RNDNUM.Next(1, 10);
+            } else if (gameMode =='-')
+            {
+                iNumberTwo = RNDNUM.Next(0, iNumberOne);
+            }
+            else
+            {
+                iNumberTwo = RNDNUM.Next(0, 10);
+            }        
+            
+        }
+        /// <summary>
+        /// Check to see If players Guess is correct If so add to total Score. 
+        /// </summary>
+        /// <param name="PlayerGuess"></param>
+        public void correctAnswer(int PlayerGuess)
+        {
+            if(PlayerGuess == iAnswer)
+            {
+                Player.iTotalCorrect++;
+            }
+
+        }
+
+        /// <summary>
+        /// Generates the answer to the specific equation. Based on the Game Mode. 
+        /// </summary>
+        public void MathFunction()
+        {
+            GenerateNumbers();
+            if (gameMode == '+')
+            {
+                iAnswer = iNumberOne + iNumberTwo; 
+            }else if(gameMode == '-')
+            {
+                iAnswer = iNumberOne - iNumberTwo; 
+            }else if(gameMode == '*')
+            {
+                iAnswer = iNumberOne * iNumberTwo;
+            }
+            else
+            {
+                iAnswer = iNumberOne / iNumberTwo; 
+            } 
+        }
+
+       
+    
+    }
+}
